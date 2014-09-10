b24f1aca5b8a7553d2797c6a6f093b4ba4913338
whiteages <whiteages@gmail.com>
2014-09-10 18:32:50 +0200

    wifi signal strength phonegap app
    wifi signal strength phonegap app

---
+++ www/js/index.js
@@ -0,0 +1,50 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+		checkConnection();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+
+
+
+
+
+function checkConnection() {
+	var somerand = document.getElementById("somerand");
+	somerand.innerHTML += Math.floor(Math.random()*100 + 100);
+	somerand.innerHTML += "<br>Wifi List:";
+	//somerand.innerHTML += window.wifi.networks;
+	
+	//alert(window.wifi);
+    //var wifiList = window.wifi.networks;
+    //alert('Wifi List: ' + wifiList);
+}
+
